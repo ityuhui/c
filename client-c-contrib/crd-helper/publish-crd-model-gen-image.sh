0251abc17faa34fd80451f53cbbbb41758288848
Hui Yu <ityuhui@gmail.com>
2020-10-21 21:30:37 +0800

    1021-1

--- client-c-contrib/crd-helper/publish-crd-model-gen-image.sh
+++ client-c-contrib/crd-helper/publish-crd-model-gen-image.sh
@@ -5,6 +5,6 @@ IMAGE_NAME=kubernetes-client/c/crd-model-gen
 IMAGE_VERSION=v1.0.0
 IMAGE=${IMAGE_REPOSITORY}/${IMAGE_NAME}:${IMAGE_VERSION}
 
-docker build -t ${IMAGE} .
+docker build . -t ${IMAGE} --network host --build-arg HTTP_PROXY=http://192.168.22.110:7071 --build-arg HTTPS_PROXY=http://192.168.22.110:7071
 
-docker push ${IMAGE}
+#docker push ${IMAGE}
